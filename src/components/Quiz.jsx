4546e0303eb75727df9602e1f87351a789b52830
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-26 17:29:46 -0300

    adding timer and logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const Quiz = () => {
-    return (
-        <div className="quiz">
-            <div className="question">Whats your color?</div>
-            <div className="answers">
-                <div className="answer correct">Rojo</div>
-                <div className="answer">Verde</div>
-                <div className="answer">Naranja</div>
-                <div className="answer">Amarillo</div>
-            </div>
-        </div>
-    )
-}
+const Quiz = ({ data, setStop, questionNumber, setQuestionNumber }) => {
+  const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
 
-export default Quiz
+  useEffect(() => {
+    setQuestion(data[questionNumber - 1]);
+  }, [data, questionNumber]);
+
+  const delay = (duration, cb) => {
+    setTimeout(() => {
+      cb();
+    }, duration);
+  };
+
+  const handleClick = (a) => {
+    setSelectedAnswer(a);
+    setClassName("answer active");
+    delay(3000, () =>
+      setClassName(a.correct ? "answer correct" : "answer wrong")
+    );
+    delay(6000, () => {
+        if (a.correct) {
+            setQuestionNumber((prev) => prev + 1);
+            setSelectedAnswer(null)
+        } else {
+            setStop(true);
+        }
+    });
+  };
+
+  return (
+    <div className="quiz">
+      <div className="question">{question?.question}</div>
+      <div className="answers">
+        {question?.answers.map((a) => (
+          <div
+            className={selectedAnswer === a ? className : "answer"}
+            onClick={() => handleClick(a)}
+          >
+            {a.text}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;

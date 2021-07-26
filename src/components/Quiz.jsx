4ddb941f3fcb78029145f809739f85cae9e2e647
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-26 16:23:14 -0300

    adding component and styles

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Quiz = () => {
+    return (
+        <div className="quiz">
+            <div className="question">Whats your color?</div>
+            <div className="answers">
+                <div className="answer correct">Rojo</div>
+                <div className="answer">Verde</div>
+                <div className="answer">Naranja</div>
+                <div className="answer">Amarillo</div>
+            </div>
+        </div>
+    )
+}
+
+export default Quiz

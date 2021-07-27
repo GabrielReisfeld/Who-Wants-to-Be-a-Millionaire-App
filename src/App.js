0a8b86b5d325b0e8861222dd717e05c9a4d7f78b
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-27 16:23:46 -0300

    adding questions

--- src/App.js
+++ src/App.js
@@ -12,29 +12,29 @@ function App() {
   const data = [
     {
       id: 1,
-      question: "Rolex is a company that specializes in what type of product?",
+      question: "What year was McDonald's founded?",
       answers: [
         {
-          text: "Phone",
+          text: "1950",
           correct: false,
         },
         {
-          text: "Watches",
+          text: "1955",
           correct: true,
         },
         {
-          text: "Food",
+          text: "1960",
           correct: false,
         },
         {
-          text: "Cosmetic",
+          text: "1965",
           correct: false,
         },
       ],
     },
     {
       id: 2,
-      question: "When did the website `Facebook` launch?",
+      question: "When did the website 'Facebook' launch?",
       answers: [
         {
           text: "2004",
@@ -56,23 +56,287 @@ function App() {
     },
     {
       id: 3,
-      question: "Who played the character of harry potter in movie?",
+      question: "Who invented the printing press?",
       answers: [
         {
-          text: "Johnny Deep",
+          text: "Benjamin Franklin",
           correct: false,
         },
         {
-          text: "Leonardo Di Caprio",
+          text: "Nikola Tesla",
           correct: false,
         },
         {
-          text: "Denzel Washington",
+          text: "George Washington",
           correct: false,
         },
         {
-          text: "Daniel Red Cliff",
+          text: "Johannes Gutenberg",
           correct: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: "What is the largest organ of the human body?",
+      answers: [
+        {
+          text: "Heart",
+          correct: false,
+        },
+        {
+          text: "Skin",
+          correct: true,
+        },
+        {
+          text: "Liver",
+          correct: false,
+        },
+        {
+          text: "Intestine",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 5,
+      question: "What is the name of the Jewish New Year?",
+      answers: [
+        {
+          text: "Elul",
+          correct: false,
+        },
+        {
+          text: "Succoss",
+          correct: false,
+        },
+        {
+          text: "Pesaj",
+          correct: false,
+        },
+        {
+          text: "Rosh Hashanah",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: "How many colors are in a rainbow?",
+      answers: [
+        {
+          text: "7",
+          correct: true,
+        },
+        {
+          text: "8",
+          correct: false,
+        },
+        {
+          text: "9",
+          correct: false,
+        },
+        {
+          text: "10",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 7,
+      question: "Which of these colors is NOT featured in the Google logo?",
+      answers: [
+        {
+          text: "Yellow",
+          correct: false,
+        },
+        {
+          text: "Blue",
+          correct: false,
+        },
+        {
+          text: "Orange",
+          correct: true,
+        },
+        {
+          text: "Red",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 8,
+      question: "What is the French word for 'hat'?",
+      answers: [
+        {
+          text: "Chapeu",
+          correct: true,
+        },
+        {
+          text: "Bonnet",
+          correct: false,
+        },
+        {
+          text: "Sombrero",
+          correct: false,
+        },
+        {
+          text: "Casque",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 9,
+      question: "Who is depicted on the US hundred dollar bill?",
+      answers: [
+        {
+          text: "Benjamin Franklin",
+          correct: true,
+        },
+        {
+          text: "George Washington",
+          correct: false,
+        },
+        {
+          text: "Abraham Lincoln",
+          correct: false,
+        },
+        {
+          text: "Thomas Jefferson",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 10,
+      question: "Who invented the lightbulb?",
+      answers: [
+        {
+          text: "Thomas Edison",
+          correct: true,
+        },
+        {
+          text: "George Washington",
+          correct: false,
+        },
+        {
+          text: "Nikola Tesla",
+          correct: false,
+        },
+        {
+          text: "Benjamin Franklin",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 11,
+      question: "In which continent is Belarus located?",
+      answers: [
+        {
+          text: "Asia",
+          correct: false,
+        },
+        {
+          text: "Africa",
+          correct: false,
+        },
+        {
+          text: "Europe",
+          correct: true,
+        },
+        {
+          text: "America",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 12,
+      question: "Which planet is the closest to the sun?",
+      answers: [
+        {
+          text: "Jupiter",
+          correct: false,
+        },
+        {
+          text: "Mars",
+          correct: false,
+        },
+        {
+          text: "Earth",
+          correct: false,
+        },
+        {
+          text: "Mercury",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 13,
+      question: "How tall is the Burj Khalifa?",
+      answers: [
+        {
+          text: "628m",
+          correct: false,
+        },
+        {
+          text: "728m",
+          correct: false,
+        },
+        {
+          text: "828m",
+          correct: true,
+        },
+        {
+          text: "928m",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 14,
+      question: "Which song doesn´t belong to 'The Beatles'?",
+      answers: [
+        {
+          text: "Come Together",
+          correct: false,
+        },
+        {
+          text: "Here Comes the Sun",
+          correct: false,
+        },
+        {
+          text: "Eleanor Rigby",
+          correct: false,
+        },
+        {
+          text: "Angie",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 15,
+      question: "What is 'Homer J. Simpson' middle name?",
+      answers: [
+        {
+          text: "James",
+          correct: false,
+        },
+        {
+          text: "Jay",
+          correct: true,
+        },
+        {
+          text: "Joseph",
+          correct: false,
+        },
+        {
+          text: "Jacob",
+          correct: false,
         },
       ],
     },
@@ -109,7 +373,7 @@ function App() {
     <div className="App">
       <div className="main">
         {stop ? (
-          <h1 className="endText">You earned: {earned}</h1>
+          <h1 className="endText">You win: {earned}</h1>
         ) : (
           <>
             <div className="top">

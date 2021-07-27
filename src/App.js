b6da1dcbec46d4e133a7ae5a07237ca8faa6ad85
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-27 14:28:25 -0300

    added logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useMemo } from "react";
 import "./App.css";
+import logo from './media/logo.png';
 import Quiz from "./components/Quiz";
 import Timer from "./components/Timer";
 
@@ -84,17 +85,17 @@ function App() {
         { id: 2, amount: "$ 200" },
         { id: 3, amount: "$ 300" },
         { id: 4, amount: "$ 500" },
-        { id: 5, amount: "$ 1000" },
-        { id: 6, amount: "$ 2000" },
-        { id: 7, amount: "$ 4000" },
-        { id: 8, amount: "$ 8000" },
-        { id: 9, amount: "$ 16000" },
-        { id: 10, amount: "$ 32000" },
-        { id: 11, amount: "$ 64000" },
-        { id: 12, amount: "$ 125000" },
-        { id: 13, amount: "$ 250000" },
-        { id: 14, amount: "$ 500000" },
-        { id: 15, amount: "$ 1000000" },
+        { id: 5, amount: "$ 1.000" },
+        { id: 6, amount: "$ 2.000" },
+        { id: 7, amount: "$ 4.000" },
+        { id: 8, amount: "$ 8.000" },
+        { id: 9, amount: "$ 16.000" },
+        { id: 10, amount: "$ 32.000" },
+        { id: 11, amount: "$ 64.000" },
+        { id: 12, amount: "$ 125.000" },
+        { id: 13, amount: "$ 250.000" },
+        { id: 14, amount: "$ 500.000" },
+        { id: 15, amount: "$ 1.000.000" },
       ].reverse(),
     []
   );
@@ -129,6 +130,7 @@ function App() {
       </div>
       <div className="pyramid">
         <ul className="moneyList">
+          <img src={logo} className="logo" alt="logo" />
           {moneyPyramid.map((m) => (
             <li
               className={
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,20 +1,25 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import wrong from "../media/wrong.mp3";
 
 const Timer = ({ setStop, questionNumber }) => {
   const [timer, setTimer] = useState(30);
+  const [wrongAnswer] = useSound(wrong);
 
   useEffect(() => {
-    if (timer === 0) return setStop(true);
+    if (timer === 0) {
+      wrongAnswer();
+      return setStop(true);
+    }
     const interval = setInterval(() => {
       setTimer((prev) => prev - 1);
     }, 1000);
     return () => clearInterval(interval);
-  }, [setStop, timer]);
+  }, [wrongAnswer, setStop, timer]);
 
   useEffect(() => {
     setTimer(30);
   }, [questionNumber]);
-
 
   return timer;
 };
